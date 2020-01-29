dc94cfda5bfcd51db3398113caa4c8d3368ddf2e
Paul Nemec <paul.nemec99@gmail.com>
2020-01-29 09:38:27 +0100

    Minimax now fully functioning

--- minimax.py
+++ minimax.py
@@ -1,60 +1,56 @@
 from TikTakToe.tiktaktoe import TikTakToe
-from random import shuffle
 import numpy as np
-from anytree import AnyNode, PreOrderIter, PostOrderIter
 
 
 class MiniMax:
-    def __init__(self, game=None):
-        if isinstance(game, TikTakToe):
-            self.game = game
-        else:
-            raise TypeError(f"game must be a game of TikTakToe, not {type(game)}")
-
-        self.me = self.game.player  # which player am I
-        self.root = AnyNode(pos=None)
-
+    def __init__(self):
         self.rate_dict = {}
+        self.optimal = {
+            1: max,
+            -1: min
+        }
+        self.positions_evaluated = 0
+        self.branch_difficulty_weighting = 0.1
 
-        self.root.rating = self.rate(game=self.game, node=self.root)
-
-    def rate(self, game, node):
-        try:
+    def rate(self, game):
+        if str(game) in self.rate_dict:
             value = self.rate_dict[str(game)]
-        except KeyError:
-            l = list(game.possible_moves)
-            shuffle(l)
-            if game.winner is not None:
-                value = game.winner
+        elif game.winner is not None:
+            value = game.winner * (len(game.zero_positions) + 1)
+        else:
+            ratings = []
+            for pos in game.zero_positions:
+                game.mark(pos)
+                r = self.rate(game=game)
+                game.un_mark(pos)
 
-            elif game.player == 1:
-                value = -1
-                for pos in l:
-                    game.mark(pos)
-                    r = self.rate(game=game, node=AnyNode(pos=pos, parent=node))
-                    game.un_mark(pos)
+                ratings.append(r)
 
-                    value = max(value, r)
+            value = self.optimal[game.player](ratings)
 
-                    if value == 1:
-                        break
+            branch_difficulty = sum(x * game.player < 0 for x in ratings) / len(ratings)
+            value -= game.player * self.branch_difficulty_weighting * branch_difficulty
 
-            else:
-                value = 1
-                for pos in l:
-                    game.mark(pos)
-                    r = self.rate(game=game, node=AnyNode(pos=pos, parent=node))
-                    game.un_mark(pos)
+        self.rate_dict[str(game)] = value
 
-                    value = min(value, r)
+        # Use symmetry (flip and three rotations)
+        self.rate_dict[str(np.rot90(game.board, k=1))] = value
+        self.rate_dict[str(np.rot90(game.board, k=2))] = value
+        self.rate_dict[str(np.rot90(game.board, k=3))] = value
 
-                    if value == -1:
-                        break
+        flipped_board = game.board.T
+        self.rate_dict[str(flipped_board)] = value
+        self.rate_dict[str(np.rot90(flipped_board, k=1))] = value
+        self.rate_dict[str(np.rot90(flipped_board, k=2))] = value
+        self.rate_dict[str(np.rot90(flipped_board, k=3))] = value
 
-            self.rate_dict[str(game)] = value
+        self.positions_evaluated += 1
+        return value
 
-        node.rating = value
-        return node.rating
+    def best_move(self, board):
+        game = TikTakToe(board)
+        pos_list = [(pos, self.rate(game.mark(pos, inplace=False))) for pos in game.zero_positions]
+        return self.optimal[game.player](pos_list, key=lambda tup: tup[1])[0]
 
 
 if __name__ == "__main__":
